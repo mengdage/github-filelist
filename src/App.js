34516538a19215d778fde53b068049198cc3e632
meng <wulalameng@gmail.com>
2017-08-27 22:57:16 -0400

    use sub-components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import FileList from './FileList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FileList files={this.props.files}/>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  files: PropTypes.array
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: "2017-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: "2017-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: "2017-07-18 21:24:00",
+    latestCommit: {
+      message: 'Added a readme'
+    }
+  },
+];
+
+ReactDOM.render(<App files={testFiles}/>, document.getElementById('root'));
